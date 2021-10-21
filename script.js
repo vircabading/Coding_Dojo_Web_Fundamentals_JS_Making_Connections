0953fb678a4245c803b0adb008ae202ce8a0761d
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-21 08:44:33 -0700

    add edit profile name function

--- script.js
+++ script.js
@@ -3,3 +3,11 @@
 // By:      Virgilio D. Cabading Jr.
 //////////////////////////////////////////////////
 
+function renameUser() {
+    console.log ("lkj;loj;ojk;ljk;lj;ljk")
+    var x = document.querySelector("#user_name");
+    console.log(x.innerHTML);
+    // x.innerHTML = "Marie Jane"
+    document.getElementById("user_name").innerHTML = document.getElementById("enter_new_profile_name").value;
+}
+
