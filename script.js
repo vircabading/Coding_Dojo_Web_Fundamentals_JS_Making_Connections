8638a18ba199eb990ddc1edd784666e9b2bed0f5
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-21 11:49:46 -0700

    add removeConnection javascript

--- script.js
+++ script.js
@@ -3,11 +3,17 @@
 // By:      Virgilio D. Cabading Jr.
 //////////////////////////////////////////////////
 
-function renameUser() {
-    console.log ("lkj;loj;ojk;ljk;lj;ljk")
-    var x = document.querySelector("#user_name");
-    console.log(x.innerHTML);
-    // x.innerHTML = "Marie Jane"
-    document.getElementById("user_name").innerHTML = document.getElementById("enter_new_profile_name").value;
+function renameUser() {                                                         // Function that renames the User
+
+    var tempElement = document.getElementById("enter_new_profile_name");
+    document.getElementById("user_name").innerHTML = tempElement.value;         // Assign new profile name
+    tempElement.value = "Enter New Profile Name";                               // Clear out input field again
 }
 
+function removeConnection (element) {
+    var tempID = element.id;
+    tempID = parseInt(tempID);                                                        // Retrieve the connection id
+    console.log("In removeConnection function, tempID is:", tempID);
+
+    document.getElementById(tempID + "-connection").remove();
+}
